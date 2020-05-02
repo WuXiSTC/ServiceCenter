67567b38c4fbade36b77d85c3843df6b5f619951
yindaheng98 <yindaheng98@163.com>
2020-05-02 22:22:53 +0800

    数据库操作接口已测试完成

--- src/test/java/cn/org/wxstc/services/ServicesApplicationTests.java
+++ src/test/java/cn/org/wxstc/services/ServicesApplicationTests.java
@@ -1,7 +1,14 @@
 package cn.org.wxstc.services;
 
+import cn.org.wxstc.services.repository.DatabaseRepository;
+import cn.org.wxstc.services.repository.RedisRepository;
+import cn.org.wxstc.services.repository.TestRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootTest
 class ServicesApplicationTests {
@@ -10,4 +17,26 @@ class ServicesApplicationTests {
     void contextLoads() {
     }
 
+    @Autowired
+    DatabaseRepository databaseRepository;
+
+    @Test
+    void databaseTests() {
+        cn.org.wxstc.services.entity.Test test =
+                new cn.org.wxstc.services.entity.Test("test_for_test", "test1", "/a/b/c");
+        databaseRepository.insert(test);
+        UUID uuid = test.getID();
+        Optional<cn.org.wxstc.services.entity.Test> otest = databaseRepository.findByID(uuid);
+        System.out.println(otest.toString());
+        for (cn.org.wxstc.services.entity.Test ot : databaseRepository.findAll())
+            System.out.println(ot.toString());
+    }
+
+    @Autowired
+    RedisRepository redisRepository;
+
+    @Test
+    void redisTests(){
+
+    }
 }
