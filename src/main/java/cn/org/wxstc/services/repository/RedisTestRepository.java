92b6a0efb461253d2481e84249a4cbeee19500ea
yindaheng98 <yindaheng98@163.com>
2020-05-02 23:39:39 +0800

    Test缓存完成

--- src/main/java/cn/org/wxstc/services/repository/RedisTestRepository.java
+++ src/main/java/cn/org/wxstc/services/repository/RedisTestRepository.java
@@ -3,5 +3,9 @@ package cn.org.wxstc.services.repository;
 import cn.org.wxstc.services.entity.Test;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface RedisTestRepository extends CrudRepository<Test, String> {
+    Optional<Test> findByID(UUID ID);
 }
