41975740f2215dbb2426155e53d31f2f26026da3
yindaheng98 <yindaheng98@163.com>
2020-05-02 16:51:41 +0800

    Initial Commit

---
+++ src/test/java/cn/org/wxstc/services/ServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.org.wxstc.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
