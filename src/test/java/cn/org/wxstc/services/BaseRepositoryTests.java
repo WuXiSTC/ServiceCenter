a467cf3381c96a9dfd4108220e0b67b25b0ac254
yindaheng98 <yindaheng98@163.com>
2020-05-03 13:12:14 +0800

    测试小改

--- src/test/java/cn/org/wxstc/services/BaseRepositoryTests.java
+++ src/test/java/cn/org/wxstc/services/BaseRepositoryTests.java
@@ -1,10 +1,10 @@
 package cn.org.wxstc.services;
 
+import cn.org.wxstc.services.entity.Test;
+import cn.org.wxstc.services.entity.User;
 import cn.org.wxstc.services.repository.DatabaseRepository;
 import cn.org.wxstc.services.repository.RedisTestRepository;
 import cn.org.wxstc.services.repository.RedisUserRepository;
-import cn.org.wxstc.services.repository.TestRepository;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,53 +16,51 @@ class BaseRepositoryTests {
     @Autowired
     DatabaseRepository databaseRepository;
 
-    @Test
+    @org.junit.jupiter.api.Test
     void databaseTests() {
-        cn.org.wxstc.services.entity.Test test =
-                new cn.org.wxstc.services.entity.Test("test_for_test", "test1", "/a/b/c");
+        Test test = new Test("test_for_test", "test1", "/a/b/c");
         databaseRepository.insert(test);
         UUID uuid = test.getID();
-        Optional<cn.org.wxstc.services.entity.Test> otest = databaseRepository.findByID(uuid);
+        Optional<Test> otest = databaseRepository.findByID(uuid);
         System.out.println(otest.toString());
-        for (cn.org.wxstc.services.entity.Test ot : databaseRepository.findAll())
+        for (Test ot : databaseRepository.findAll())
             System.out.println(ot.toString());
-        for (cn.org.wxstc.services.entity.Test ot : databaseRepository.findAllByUSER("test1"))
+        for (Test ot : databaseRepository.findAllByUSER("test1"))
             System.out.println(ot.toString());
     }
 
     @Autowired
     RedisUserRepository redisUserRepository;
 
-    @Test
+    @org.junit.jupiter.api.Test
     void redisUserTests() {
         Map<String, SortedMap<UUID, String>> uuids = new HashMap<>();
-        for (cn.org.wxstc.services.entity.Test ot : databaseRepository.findAll()) {
+        for (Test ot : databaseRepository.findAll()) {
             SortedMap<UUID, String> uids = uuids.get(ot.getUSER());
             if (uids == null) uids = new TreeMap<>();
             uids.put(ot.getID(), ot.getName());
             uuids.put(ot.getUSER(), uids);
         }
         for (Map.Entry<String, SortedMap<UUID, String>> entry : uuids.entrySet()) {
-            cn.org.wxstc.services.entity.User user =
-                    new cn.org.wxstc.services.entity.User(entry.getKey(), entry.getValue());
+            User user = new User(entry.getKey(), entry.getValue());
             redisUserRepository.save(user);
         }
-        for (cn.org.wxstc.services.entity.User u : redisUserRepository.findAll())
+        for (User u : redisUserRepository.findAll())
             System.out.println(u.toString());
     }
 
     @Autowired
     RedisTestRepository redisTestRepository;
 
-    @Test
+    @org.junit.jupiter.api.Test
     void redisTestTests() {
-        cn.org.wxstc.services.entity.Test test =
-                new cn.org.wxstc.services.entity.Test("test-" + UUID.randomUUID(), "test1", "/a/b/c");
+        Test test =
+                new Test("test-" + UUID.randomUUID(), "test1", "/a/b/c");
         redisTestRepository.save(test);
         UUID uuid = test.getID();
-        Optional<cn.org.wxstc.services.entity.Test> otest = redisTestRepository.findByID(uuid);
+        Optional<Test> otest = redisTestRepository.findByID(uuid);
         System.out.println(otest.toString());
-        for (cn.org.wxstc.services.entity.Test ot : redisTestRepository.findAll())
+        for (Test ot : redisTestRepository.findAll())
             System.out.println(ot.toString());
     }
 }
