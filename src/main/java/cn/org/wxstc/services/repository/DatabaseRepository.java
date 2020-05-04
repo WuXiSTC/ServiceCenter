ff7d4137e4788ffbb9c498afb296f423705883fe
yindaheng98 <yindaheng98@163.com>
2020-05-04 11:25:59 +0800

    数据操作设计图完成

--- src/main/java/cn/org/wxstc/services/repository/DatabaseRepository.java
+++ src/main/java/cn/org/wxstc/services/repository/DatabaseRepository.java
@@ -2,7 +2,6 @@ package cn.org.wxstc.services.repository;
 
 import cn.org.wxstc.services.entity.Test;
 import org.springframework.data.cassandra.repository.CassandraRepository;
-import org.springframework.data.repository.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
--- src/main/java/cn/org/wxstc/services/repository/RedisUserRepository.java
+++ src/main/java/cn/org/wxstc/services/repository/RedisUserRepository.java
@@ -1,11 +1,9 @@
 package cn.org.wxstc.services.repository;
 
-import cn.org.wxstc.services.entity.Test;
 import cn.org.wxstc.services.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 public interface RedisUserRepository extends CrudRepository<User, String> {
     Optional<User> findByID(String ID);
